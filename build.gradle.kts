1295c880f02b44b4dd39c19ee24b344c7262b21d
rexy <rexy.adrian.f@gmail.com>
2026-02-24 22:09:15 +0700

    fix sonar org key

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,7 +81,7 @@ tasks.jacocoTestReport {
 sonar {
     properties {
         property("sonar.projectKey", "A-Rexy-Adrian-F-2406495666_Modul-1-Coding-Standards")
-        property("sonar.organization", "A-Rexy Adrian F-2406495666")
+        property("sonar.organization", "a-rexy-adrian-f-2406495666")
         property("sonar.host.url", "https://sonarcloud.io")
     }
 }
